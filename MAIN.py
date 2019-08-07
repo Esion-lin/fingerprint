2be9a9267fe78a70b6a9af7842e897f3936efd16
esion-lin <bigesion@gmail.com>
2019-08-07 20:51:39 +0800

    update

---
+++ MAIN.py
@@ -0,0 +1,65 @@
+from finger2path import finger2path
+import argparse
+threshold = 1
+test = True
+def get_parser():
+	parser = argparse.ArgumentParser(description='parameters of datasets')
+	parser.add_argument('--file_dir', default='~', help='dir of all fp files')
+	parser.add_argument('--points_num', default=5, help='how many points a path contain')
+	args = parser.parse_args()
+	return args
+
+def compare_hash(paths1,paths2):
+	sum = 0
+	for ele in paths2:
+		if ele in paths1:
+			sum += 1
+	return sum#*2.0/(len(paths1)+len(paths2))
+
+def inside_class(num_i, thr = threshold):
+	ans = set()
+	summ = 0
+	for i in range(1,7):
+		ans = ans | finger2path(args.file_dir+'/%d_%d.fp'%(num_i,i),args.points_num)
+	for i in range(7,9):
+		num = compare_hash(ans,finger2path(args.file_dir+'/%d_%d.fp'%(num_i,i),args.points_num))
+		if test:
+			print("%d_%d sum is:" %(num_i,i),num)
+		if num >= thr:
+			summ += 1
+	return summ
+
+def outside_class(num_i, thr = threshold, range_of_id = range(1,101)):
+	ans = set()
+	summ = 0
+	for i in range(1,7):
+		ans = ans | finger2path(args.file_dir+'/%d_%d.fp'%(num_i,i),args.points_num)
+	for i in range(1,9):
+		for j in range_of_id:
+			if i == j:
+				continue
+			else:
+				num = compare_hash(ans,finger2path(args.file_dir+'/%d_%d.fp'%(j,i),args.points_num))
+				if test:
+					print("%d_%d num is:" %(j,i),num)
+				if num < thr:
+					summ += 1
+	return summ
+
+if __name__ == '__main__':
+	args = get_parser()
+	
+	sum1 = 0
+	sum2 = 0
+	# test inside class
+	for i in range(1,101):
+		sum1 += inside_class(i)
+	print("TA: ", sum1*1.0/100/2)
+	'''
+	# test outside class
+	
+	for i in range(1,101):
+		print("sum is ",outside_class(i))
+	#print(get_list(args.file_dir))
+
+	'''
---
+++ finger2path.py
@@ -0,0 +1,101 @@
+import argparse
+import numpy as np
+import os
+
+test = False
+
+def mapping(angle):
+	return int(angle/45)
+def mapping_dis(distance):
+	return int(distance/10)
+def get_parser():
+	parser = argparse.ArgumentParser(description='parameters of datasets')
+	parser.add_argument('--file_dir', default='~', help='dir of all fp files')
+	parser.add_argument('--points_num', default=5, help='how many points a path contain')
+	args = parser.parse_args()
+	return args
+
+def distance(tuple1, tuple2):
+	return ((tuple1[1] - tuple2[1])**2 + (tuple1[0] - tuple2[0])**2)**0.5
+	#0:x 1:y 2:angle
+
+def insight_angle(tuple1, tuple2, tuple3):
+	a = np.array([tuple1[0] - tuple2[0], tuple1[1] - tuple2[1]])
+	b = np.array([tuple3[0] - tuple2[0], tuple3[1] - tuple2[1]])
+	La=np.sqrt(a.dot(a))
+	Lb=np.sqrt(b.dot(b))
+	if La == 0 or Lb == 0:
+		return 90
+	cos_angle=a.dot(b)/(La*Lb)
+	if cos_angle > 1:
+		cos_angle = 1
+	if cos_angle < -1:
+		cos_angle = -1
+	return mapping(np.arccos(cos_angle)*360/2/np.pi)
+
+def cop_sort(tuple,tuple_ele):
+	new_list = sorted(tuple,key=lambda x:distance(x,tuple_ele))
+	return new_list
+
+
+def make_path(minutiaes):
+	dis = []
+	phi = []
+	sigma = []
+	for i in range(len(minutiaes) - 2):
+		dis.append(int(distance(minutiaes[i],minutiaes[i+1])))
+		phi.append(int(insight_angle(minutiaes[i],minutiaes[i+1],minutiaes[i+2])))
+		sigma.append(int(minutiaes[i+1][2] - minutiaes[i][2])%360)
+	dis.append(int(distance(minutiaes[len(minutiaes) - 2],minutiaes[len(minutiaes) - 1])))
+	sigma.append(int(minutiaes[len(minutiaes) - 2][2] - minutiaes[len(minutiaes) - 1][2])%360)
+	return dis + phi + sigma
+
+def make_path_str(minutiaes):
+	dis = ''
+	phi = ''
+	sigma = ''
+	for i in range(len(minutiaes) - 2):
+		dis += str(mapping_dis(int(distance(minutiaes[i],minutiaes[i+1]))))
+		if test:
+			print('distance is ', distance(minutiaes[i],minutiaes[i+1]))
+		phi += str(int(insight_angle(minutiaes[i],minutiaes[i+1],minutiaes[i+2])))
+		sigma += str(mapping(int(minutiaes[i+1][2] - minutiaes[i][2])%360))
+	dis += str(mapping_dis(int(distance(minutiaes[len(minutiaes) - 2],minutiaes[len(minutiaes) - 1]))))
+	sigma += str(mapping(int(minutiaes[len(minutiaes) - 2][2] - minutiaes[len(minutiaes) - 1][2])%360))
+	return dis + phi + sigma
+
+def finger2path(file,points_num):
+	path = set()
+	with open(file,'r') as reader:
+		minutiaes = []
+		while True:
+			line = reader.readline()
+			if line.startswith('[minutiae]'):
+				break
+		num = int(reader.readline())
+		for _ in range(num):
+			line = reader.readline()
+			minutiaes.append([int(x) for x in line.split()[:3]])
+	for ele in minutiaes:
+		minutiaes_copy = cop_sort(minutiaes,ele)
+		path.add(make_path_str(minutiaes_copy[:points_num]))
+	''' for test break'''
+	if test:
+		print(path)
+	''' test end '''
+	return path
+
+def bat_deal_files(filespath, points_num):
+	files = os.listdir(filespath)
+	for file in files:
+		finger2path(filespath + '/' + file, points_num)
+
+
+
+def test_this_file(filespath):
+	get_list(filespath)
+	pass
+if __name__ == '__main__':
+	args = get_parser()
+	finger2path(args.file_dir+'/1_1.fp',args.points_num)
+	#print(get_list(args.file_dir))
